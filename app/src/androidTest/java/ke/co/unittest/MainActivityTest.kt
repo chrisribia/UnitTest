0674a793799cdf825739ff5015f1849442f1d0c5
chris <chrisribia@gmail.com>
2019-12-04 12:42:41 +0300

    test backpress button functionality

--- app/src/androidTest/java/ke/co/unittest/MainActivityTest.kt
+++ app/src/androidTest/java/ke/co/unittest/MainActivityTest.kt
@@ -2,6 +2,7 @@ package ke.co.unittest
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -21,6 +22,22 @@ class MainActivityTest{
         onView(withId(R.id.secondary))
             .check(matches(isDisplayed()))
     }
+
+    @Test
+    fun test_backpress_ToActivity(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.button))
+            .check(matches(isDisplayed())).perform((click()))
+
+        onView(withId(R.id.secondary))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.button2))
+            .perform(click())//method 1
+       // pressBack()//method 2
+
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
+    }
+
 
 
 
